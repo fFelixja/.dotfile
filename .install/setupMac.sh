e3b6c1e27d874d182b32ec0c4e68a0bfde120961
Felix Jansson <felixjansson.96@gmail.com>
2017-01-26 21:05:34 +0100

    Hot fix

--- .install/setupMac.sh
+++ .install/setupMac.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+echo "Hello, please wait for xCode to install... :(" 
 #%%%%%%%%%%%%%%%%%
 #    mkdirs	 %
 #%%%%%%%%%%%%%%%%%
-if [ ! -d ~/develope ] then
+if [ ! -d ~/develope ]; then
 	mkdir ~/develope
 fi
 
-if [ ! -d ~.pubSrc  ]
+if [ ! -d ~.pubSrc  ]; then
 	mkdir ~/.pubSrc
 fi
+
 #%%%%%%%%%%%%%%%%%
 #     brew	 %
 #%%%%%%%%%%%%%%%%%
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/installer)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 
 #%%%%%%%%%%%%%%%%%
 # brew some beer %
 #%%%%%%%%%%%%%%%%%
-brew install cask java wget spotify
-brew install git
+brew install cask  wget spotify
+brew install java git
 
