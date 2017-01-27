870c1f4b5d668f8c15a17c0b35537ce03ebf61a4
Felix Jansson <felixjansson.96@gmail.com>
2017-01-27 15:25:40 +0100

    iterm2 setup + font

--- .install/brewSomeMac.sh
+++ .install/brewSomeMac.sh
@@ -14,4 +14,5 @@ brew install git android-sdk
 #%%%%%%%%%%%%%%%%%
 #   oh-my-zsh    %
 #%%%%%%%%%%%%%%%%%
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
