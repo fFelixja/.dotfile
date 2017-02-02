0de625ae86bd3cbf047b4b1c4beefa302188c59a
Felix Jansson <felixjansson.96@gmail.com>
2017-02-02 19:14:17 +0100

    removed stuff

--- .install/oh-zsh.sh
+++ .install/oh-zsh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 file=".zshrc"
 rm ~/"$file"
-
-#cp ~/.dotfile/.install/iterm2.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 echo "source ~/.dotfile/.zshrc" >> ~/"$file"
