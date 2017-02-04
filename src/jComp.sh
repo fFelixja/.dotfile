422b7235252224660a0fa5ff403802b0fd59fc5d
Felix Jansson <felixjansson.96@gmail.com>
2017-02-04 11:25:19 +0100

    jComp.sh may take compiler arguments

--- src/jComp.sh
+++ src/jComp.sh
@@ -2,19 +2,39 @@
 #This script will compile all java files directory and put *.class into ./out
 DIRECTORY=".out"
 FILEXT=$1
+ARGS=""
+FILES=""
 
-#TODO Fixa så $1 kan vara flagga för kompilatorn
+#Get all arguments to compiler
+for var in "$@"
+ do
+	if [[ (-n $var) && ($var == -*) ]]; then
+		ARGS+=" $var"
+	fi
+done
+ARGS+=" -d"
 
+#Get all classes which would be compiled
+for var in "$@"
+do
+	if [[ (-n $var) && ($var != -*) ]]; then
+		FILES+=" $var"
+	fi
+done
+
+#Create DIRECTORY .out if not exist
 if [ ! -d $DIRECTORY ]; then
 	    mkdir .out
 fi
 
-
-if [ ! -f $FILEXT  ]; then
-	echo "No "$FILEXT" file to compile"
-       	STATUS=0
-else
-	javac -d .out $FILEXT
+#Time to compile! :)
+if [ -z $FILES ]; then
+	javac $ARGS .out *.java
 	STATUS=1
+	echo "ALL FILES COMPILED"
+else
+	javac $ARGS .out $var
+	STATUS=1
+	echo "ALL GIVEN FILES COMPILED"
 fi
 exit $STATUS
